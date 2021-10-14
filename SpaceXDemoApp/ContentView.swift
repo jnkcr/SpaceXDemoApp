097d6f25c615e67644b60d315c8077ed07fe68c1
Jan Kučera <kucera.jan@hotmail.cz>
2021-10-14 20:40:43 +0200

    Initial Commit

---
+++ SpaceXDemoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpaceXDemoApp
+//
+//  Created by Jan Kučera on 14.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpaceXDemoApp/SpaceXDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXDemoAppApp.swift
+//  SpaceXDemoApp
+//
+//  Created by Jan Kučera on 14.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
