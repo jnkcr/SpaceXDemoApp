a2c57792e2045d820908b33e2e84aae17140b40b
Jan Kučera <kucera.jan@hotmail.cz>
2021-10-16 12:09:42 +0200

    Implemented all properties of LaunchModel (no coding keys yet)

--- SpaceXDemoApp/LunchModel.swift
+++ SpaceXDemoApp/LunchModel.swift
@@ -24,14 +24,14 @@ struct LunchModel: Codable, Identifiable {
     let failures: [Failures]
     let upcoming: Bool
     let details: String?
-//    let fairings
-//    let crew
+    let fairings: Fairings?
+    let crew: [Crew]
     let ships: [String?]
     let capsules: [String?]
     let payloads: [String?]
     let launchpad: String?
-//    let cores
-//    let links
+    let cores: [Core]
+    let links: Links
     let auto_update: Bool
     let success: Bool
     
@@ -50,4 +50,56 @@ struct Failures: Codable {
     let time: Int?
     let altitude: Int?
     let reason: String?
+}
+
+struct Crew: Codable {
+    let crew: String?
+    let role: String?
+}
+
+struct Core: Codable {
+    let core: String?
+    let flight: Int?
+    let gridfins: Bool?
+    let legs: Bool?
+    let reused: Bool?
+    let landing_attempt: Bool?
+    let landing_success: Bool?
+    let landing_type: String?
+    let landpad: String?
+}
+
+struct Links: Codable {
+    let patch: Patch
+    let reddit: Reddit
+    let flickr: Flickr
+    let presskit: String?
+    let webcast: String?
+    let youtube_id: String?
+    let article: String?
+    let wikipedia: String?
+}
+
+struct Patch: Codable {
+    let small: String?
+    let large: String?
+}
+
+struct Reddit: Codable {
+    let campaign: String?
+    let launch: String?
+    let media: String?
+    let recovery: String?
+}
+
+struct Flickr: Codable {
+    let small: [String?]
+    let original: [String?]
+}
+
+struct Fairings: Codable {
+    let reused: Bool?
+    let recovery_attempt: Bool?
+    let recovered: Bool?
+    let ships: [String?]
 }
